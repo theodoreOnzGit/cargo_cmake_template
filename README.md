38fe86462a43a3792061d79ea0ab5465a37ea142
teddy0@archDesktop <tong011@e.ntu.edu.sg>
2023-12-15 23:03:16 +0800

    v0.0.1 have in principle demonstration of cargo watch with cmake

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # cargo_cmake_template
 Use cargo to make life with cpp and cmake easier
+
+Basically, the cpp build tooling is not as 
+
---
+++ c_src/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "build_cmake: builds the cpp project according to CMakeLists.txt in current dir"
+build_cmake(){
+	mkdir build 
+	cmake -S . -B ./build
+	make -C ./build
+}
+
+echo "watch_cmake: uses cargo watch to watch the cmake_project 
+and run cmake"
+watch_cmake(){
+	cargo watch --ignore ./c_src/build \
+		-- cmake -S ./c_src -B ./c_src/build
+}
+
+echo "clean_cmake: cleans the build directory in current dir"
+clean_cmake(){
+	rm -r build/*
+}
