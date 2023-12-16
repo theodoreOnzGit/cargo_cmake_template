5bf55de42dad2b5ba901e0c5b3bc2292a76fd34b
teddy0@archDesktop <tong011@e.ntu.edu.sg>
2023-12-16 16:02:50 +0800

    added example of bad pointer dereferencing

--- c_src/main.cpp
+++ c_src/main.cpp
@@ -2,6 +2,12 @@
 
 #include <iostream>
 
+// invalid pointer 
+
+float* invalid_ptr(float number){
+	return &number;
+}
+
 int main() {
 	// note that \n means CR or carriage return,
 	// the equivalent of enter
@@ -11,6 +17,21 @@ int main() {
 	float c = 3.0;
 
     std::cout << c + b;
+    std::cout << "\n\n";
+	
+	// raw_pointer
+	float* c_ptr = &c;
+	std::cout << c_ptr;
+    std::cout << "\n\n";
+
+	// invalid ptr (segfault)
+	
+	float* bad_ptr = invalid_ptr(3.0);
+	std::cout << bad_ptr;
+	float bad_num = *bad_ptr;
+
+
+
     return 0;
 }
 
